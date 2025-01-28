e2273eb2ac418468a1dafd54fb9f2e8715d052e8
nakyung22 <knk655065@naver.com>
2025-01-29 05:11:21 +0900

    파이썬 시작하기

---
+++ 파이썬 시작하기.py
@@ -0,0 +1,21 @@
+print("Hello world")
+
+print("Mary's cosmetics")
+
+print('신씨가 소리질렀다. "도둑이야".')
+
+print("C:\\Windows")
+
+print("안녕하세요. \n만나서\t\t반갑습니다.")
+#\ts는 탭을 의미하고, \n은 줄바꿈을 의미합니다
+
+print("오늘은", "일요일")
+#예상 답: 오늘은 일요일
+
+print("never;""kakao;""sk;""samsung")
+
+print("never", "kakao", "sk", "samsung", sep = "/")
+
+print("first", end = ""); print("second")
+
+print(5 / 3)
