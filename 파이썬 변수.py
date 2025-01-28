9208bfe0c6e9e42ce9fba1b1acd9f716f9316b38
nakyung22 <knk655065@naver.com>
2025-01-29 05:26:05 +0900

    파이썬 변수

---
+++ 파이썬 변수.py
@@ -0,0 +1,41 @@
+삼성전자 = 50000
+총평가금액 = 삼성전자 * 10
+print(총평가금액)
+
+시가총액 = 2980000000000
+현재가 = 50000
+PER = 15.79
+print(시가총액, type(시가총액))
+print(현재가, type(현재가))
+print(PER, type(PER))
+
+s = "hello"
+t = "python"
+print(s+"!", t)
+
+print(2 + 2 * 3)
+
+a = "132"
+print(type(a))
+
+num_str = "720"
+num_int = int(num_str)
+print(num_int+1, type(num_int))
+
+num = 100
+num_str = str(num)
+print(num_str, type(num_str))
+
+num_str = "15.79"
+num_float = (float(num_str))
+print(num_float, type(num_float))
+
+year = "2020"
+result = int(year)
+print(result - 3)
+print(result - 2)
+print(result - 1)
+
+월 = 48584
+총금액 = 월 * 36
+print(총금액)
